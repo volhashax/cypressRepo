7d8425ece2bbcbb6bd08d0a33f4da46270fd6863
volhashax <volhashax@gmail.com>
2025-04-28 11:27:05 +0200

    users and selectors added

---
+++ cypress/support/selectors.js
@@ -0,0 +1,10 @@
+const loginPage = {
+  title: ".login_logo",
+  username: "[data-test=username]",
+  password: "[data-test=password]",
+  errorIcon: '[data-icon="times-circle"]',
+  login: '[data-test="login-button"]',
+  error: ".error-message-container",
+  errorMessage: '[data-test="error"]',
+  errorClose: '[data-test="error-button"]',
+};
